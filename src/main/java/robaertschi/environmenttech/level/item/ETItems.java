35f54c29caff5f5c0038f92146bebac0571b547d
Robin Bärtschi <rtmbaertschi007@gmail.com>
2024-06-02 17:37:04 +0200

    move to kotlin dsl and remove unused code

--- src/main/java/robaertschi/environmenttech/level/item/ETItems.java
+++ src/main/java/robaertschi/environmenttech/level/item/ETItems.java
@@ -2,7 +2,6 @@ package robaertschi.environmenttech.level.item;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -20,29 +19,30 @@ import static robaertschi.environmenttech.level.block.ETBlocks.EXAMPLE_BLOCK;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class ETItems {
-    // Create a Deferred Register to hold Items which will all be registered under the "environmenttech" namespace
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "environmenttech" namespace
+
+
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
 
-    // Creates a new BlockItem with the id "environmenttech:example_block", combining the namespace and path
     public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
 
-    // Creates a new food item with the id "environmenttech:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
-            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+    public static final DeferredItem<EnvDetectorItem> ENV_DETECTOR_ITEM = ITEMS.registerItem("env_detector",
+            EnvDetectorItem::new,
+            new Item.Properties()
+                    .stacksTo(1)
+                    .durability(10)
+    );
 
-    public static final DeferredItem<EnvDetectorItem> ENV_DETECTOR_ITEM = ITEMS.registerItem("env_detector", EnvDetectorItem::new, new Item.Properties().stacksTo(1).durability(10));
 
-
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+    @SuppressWarnings("unused")
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CREATE_MODE_TAB = CREATIVE_MODE_TABS.register("environmenttech", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.environmenttech"))
             .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+            .icon(() -> ENV_DETECTOR_ITEM.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
                 output.accept(ENV_DETECTOR_ITEM.get());
+                output.accept(EXAMPLE_BLOCK_ITEM.get());
             }).build());
 
 
@@ -53,12 +53,10 @@ public class ETItems {
     }
 
 
+    @SuppressWarnings("unused")
     @SubscribeEvent()
     // Add the example block item to the building blocks tab
     public static void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(EXAMPLE_BLOCK_ITEM);
-        }
     }
 }
