5373cfc84f27af7e80b5d4532297c34ba42f47c3
Bärtschi Robin <Robin.Baertschi@swisscom.com>
2024-06-07 08:47:25 +0200

    Get unittests to work

---
+++ src/junit/java/robaertschi/environmenttech/unittest/TestMod.java
@@ -0,0 +1,9 @@
+package robaertschi.environmenttech.unittest;
+
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(value = TestMod.MOD_ID)
+public class TestMod {
+    public static final String MOD_ID = "environmenttech_tests";
+}
--- src/junit/java/robaertschi/environmenttech/unittest/TestProgressBar.java
+++ src/junit/java/robaertschi/environmenttech/unittest/TestProgressBar.java
@@ -1,7 +1,21 @@
 package robaertschi.environmenttech.unittest;
 
-import net.neoforged.testframework.annotation.TestGroup;
 import org.junit.jupiter.api.Test;
+import robaertschi.environmenttech.client.screen.ProgressArrowComponent;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class TestProgressBar {
+    int gsp(int progress, int maxProgress) {
+        return ProgressArrowComponent.getScaledProgress(progress, maxProgress);
+    }
+
+    @Test
+    void testScales() {
+        int scale1 = ProgressArrowComponent.getScaledProgress(1, 24);
+        assertThat(scale1).isEqualTo(1);
+        assertThat(gsp(24, 24)).isEqualTo(24);
+        assertThat(gsp(1, 12)).isEqualTo(2);
+    }
+
 }
