4b8e34d55da47ab7a627633552033f5e1651417c
Robin Bärtschi <rtmbaertschi007@gmail.com>
2024-06-27 14:25:08 +0200

    f, rebase readded the copyright header in EnvStorageRenderer.java

--- src/main/java/robaertschi/environmenttech/client/renderer/EnvStorageRenderer.java
+++ src/main/java/robaertschi/environmenttech/client/renderer/EnvStorageRenderer.java
@@ -1,9 +1,3 @@
-/*
- *  EnvironmentTech  Copyright (C) 2024 Robin Bärtschi
- *  This program comes with ABSOLUTELY NO WARRANTY; for details open the file LICENSE at the root of the source code.
- *  This is free software, and you are welcome to redistribute it
- *  under certain conditions; read the LICENSE file at the root of the source code for details.
- */
 package robaertschi.environmenttech.client.renderer;
 
 import java.util.List;
