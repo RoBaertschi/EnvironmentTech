f78e208a955e0a0af6dc9c9145e54ac790fa3004
Robin Bärtschi <rtmbaertschi007@gmail.com>
2024-06-27 14:52:45 +0200

    add missing copyright attribution for MouseUtils.java

--- src/main/java/robaertschi/environmenttech/utils/MouseUtils.java
+++ src/main/java/robaertschi/environmenttech/utils/MouseUtils.java
@@ -1,5 +1,11 @@
 package robaertschi.environmenttech.utils;
 
+/**
+ * This file is from <a href="https://github.com/Tutorials-By-Kaupenjoe/Forge-Tutorial-1.19/">here</a>.
+ * So all credits go to Kaupenjoe. The code is under the following license: <a href="https://github.com/Tutorials-By-Kaupenjoe/Forge-Tutorial-1.19/blob/main/LICENSE">here</a>
+ * @author Kaupenjoe
+ */
+
 public class MouseUtils {
     @SuppressWarnings("unused")
     public static boolean isMouseOver(double mouseX, double mouseY, int x, int y) {
