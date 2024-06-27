ba7a6bc7b60a40a4098fd90f0ddd61451337b913
Robin Bärtschi <rtmbaertschi007@gmail.com>
2024-06-27 12:48:44 +0200

    relicense to LGPL-3.0-only and CC-4.0-BY-NC-SA for assets. This excludes EnvStorageRenderer.java as it is originally from IE

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,11 +60,13 @@ spotless {
         endWithNewline()
     }
 
-    java {
-        importOrder("lombok", "java|javax", "", "net.minecraft", "com.mojang", "robaertschi", "\\#")
-        removeUnusedImports()
-        licenseHeaderFile("HEADER.java")
-    }
+	java {
+		// Originally, this was from Kaupenjoe's repo, and was licensed by BluSunrize as the original code was from her, so we exclude it to not add our License Header
+		targetExclude("src/main/java/robaertschi/environmenttech/client/renderer/EnvStorageRenderer.java")
+		importOrder("lombok", "java|javax", "", "net.minecraft", "com.mojang", "robaertschi", "\\#")
+		removeUnusedImports()
+		licenseHeaderFile("HEADER.java")
+	}
 }
 
 
